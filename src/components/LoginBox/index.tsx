2946ef7cff600f0b460e12724e1c8c89374708ab
unknown <ygorsallesaniceto@gmail.com>
2022-01-30 17:04:06 -0300

    add: Home screen

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,5 +1,14 @@
+import { VscGithubInverted } from 'react-icons/vsc';
+import styles from './styles.module.scss';
+
 export function LoginBox() {
     return (
-        <h1>LoginBox</h1>
+        <div className={styles.loginBoxWrapper}>
+            <strong>Entre e compartilhe a sua mensagem</strong>
+            <a href="#" className={styles.signInWithGithub}>
+                <VscGithubInverted size={24} />
+                Entrar com github
+            </a>
+        </div>
     )
 }
