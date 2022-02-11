cc16a99ca0633b2261cb4d020324e70e55efd0c6
unknown <ygorsallesaniceto@gmail.com>
2022-02-10 23:55:58 -0300

    refactor: environment socket-io

--- src/components/MessageList/indext.tsx
+++ src/components/MessageList/indext.tsx
@@ -14,7 +14,7 @@ type Message = {
 
 const messageQeue: Message[] = [];
 
-const socket = io('http://localhost:4000');
+const socket = io(`${import.meta.env.VITE_BASE_URL}`);
 
 socket.on('new_message', (newMessage: Message) => {
   messageQeue.push(newMessage);
